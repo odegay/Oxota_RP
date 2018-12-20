44527b0f698a7841b13bed5186c468ad4eff39d5
Oleg Degay <Oleg_Degay@epam.com>
2018-12-21 00:08:04 +0300

    Java server 2

---
+++ src/main/java/org/reguhoop/Oxotarpserver.java
@@ -0,0 +1,17 @@
+package org.reguhoop;
+
+import net.gtaun.shoebill.resource.Gamemode;
+
+public class Oxotarpserver extends Gamemode{
+    @Override
+    protected void onEnable() throws Throwable {
+        //Entry point
+        getLogger().info("TEST!!!!!");
+
+    }
+
+    @Override
+    protected void onDisable() throws Throwable {
+
+    }
+}
